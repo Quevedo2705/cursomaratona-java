36a754c605ee75a2f08623ba835765f8ca873c44
joao pedro quevedo <joaopedroquevedo321@gmail.com>
2025-10-28 22:25:44 -0300

    concluindo aula de operadores aritmeticos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main (String[] args) {
+        int numero= (6);
+        int numero2 = (10);
+       int resultado = numero + numero2;
+        System.out.println(resultado);
+
+    }
+}
+
