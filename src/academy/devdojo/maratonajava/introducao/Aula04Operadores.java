bf3ce667cdd4e614d3e7af1156a379ddbc16b84b
joao pedro quevedo <joaopedroquevedo321@gmail.com>
2025-11-01 23:07:03 -0300

    concluindo aula de logicos (AND)

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -33,6 +33,33 @@ public class Aula04Operadores {
         boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
         System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaleiMaiorQueTrinta);
         System.out.println("isDentroDaLeiMenorqueTrinta "+isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        double valorTotalGuardado = valorTotalContaCorrente + valorTotalContaPoupanca;
+        float valorPlaystation = 5800;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca >= valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel  " + isPlaystationCincoCompravel + " Valor do playstation: " + valorPlaystation + " saldo da conta corrente: " + valorTotalContaCorrente + " saldo da conta poupanca " + valorTotalContaPoupanca  + " total guardado e " + valorTotalGuardado);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // ++(adiciona 1 a variavel) --(subtrai 1 da variavel)
+        int contador =0;
+        contador += 1;
+        contador ++;
+        contador --;
+        ++contador;
+        --contador;
+        int contador2 =0;
+        System.out.println(contador2);
+        System.out.println(contador2);
     }
 }
 
