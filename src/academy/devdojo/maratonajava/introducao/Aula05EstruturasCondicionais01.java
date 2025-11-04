4ad0179df3d6efe72b0686601a1cb59a8a87c7da
joao pedro quevedo <joaopedroquevedo321@gmail.com>
2025-11-03 22:50:14 -0300

    utilizando na pratica if e else

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,17 +1,20 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
       //sempre sera um valor booleano
       //so sera executado se for true
       int idade = 15;
       boolean isAutorizadoComprarBebida = idade >=18;
+
+
       if(isAutorizadoComprarBebida){
           System.out.println("pode comprar bebidas");
+      }else{
+          System.out.println("nao pode comprar bebidas");
       }
-      if(!isAutorizadoComprarBebida){
-          System.out.println("nao pode comprar a bebida");
-      }
+
+
 
     }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        //idade <15 categoria infantil
+        //idade >=15 && idade <18 categoria juvenil
+        //idade >=18 categoria adulto
+
+        int idade = 19;
+        if (idade < 15){
+            System.out.println("categoria infantil");
+        }
+        if (idade >=15 && idade < 18){
+            System.out.println("categoria juvenil");
+        } else if (idade >=18){
+            System.out.println("categoria adulto");
+        }
+    }
+}
