f82d601d65a5ba948433403d50985eed909453f9
joao quevedo <joaopedroquevedo321@gmail.com>
2026-03-20 15:32:54 -0300

    Iniciando os estudos de Switch e praticando com exercicios

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        byte dia = 1;
+        switch (dia) {
+            case 1:
+                System.out.println("final de semana");
+                break;
+            case 2:
+                System.out.println("é dia util");
+            case 3:
+                System.out.println("é dia util");
+                break;
+            case 4:
+                System.out.println("é dia util");
+                break;
+            case 5:
+                System.out.println("é dia util");
+                break;
+            case 6:
+                System.out.println("é dia util");
+                break;
+            case 7:
+                System.out.println("é final de semana");
+                break;
+            default:
+                System.out.println("Nao e um numero valido");
+        }
+
+
+    }
+}
