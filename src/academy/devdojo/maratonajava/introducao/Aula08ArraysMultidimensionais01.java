80f131756f57a6b595bd8c8d9cbffd13f5520f40
joao quevedo <joaopedroquevedo321@gmail.com>
2026-03-24 13:20:15 -0300

    conhecendo o foreach

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -2,15 +2,34 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula08ArraysMultidimensionais01 {
     public static void main(String[] args) {
-        int[] numeros = new int[3];
-        int[] numeros2 = {2, 3, 4, 5};
-        int[] numeros3 = new int[]{1, 2, 3, 4, 5};
+        // 1,2,3,4,5 meses
+        //31,25,31,30
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 31;
+        dias[0][2] = 31;
 
-        //  for (int i = 0; i < numeros2.length; i++) {
-        //     System.out.println(numeros2[i]);
-        //  }
-        for (int num : numeros3) {
-            System.out.println(num);
+        dias[1][0] = 31;
+        dias[1][1] = 31;
+        dias[1][2] = 31;
+
+        dias[2][0] = 31;
+        dias[2][1] = 31;
+        dias[2][2] = 31;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[i].length; j++) {
+                System.out.println(dias[i][j]);
+            }
+        }
+
+        System.out.println("-----------------------------------");
+
+        for (int[] arrBase : dias) {
+            for (int num : arrBase) {
+                System.out.println(num);
+            }
         }
     }
 }
+
